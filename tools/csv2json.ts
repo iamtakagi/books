56e423ab4127ef76b8ec4cbb92cc4c24b8ec1a87
iamtakagi <github@iamtakagi.net>
2023-05-14 03:56:22 +0900

    format: prettier

--- tools/csv2json.ts
+++ tools/csv2json.ts
@@ -1,25 +1,28 @@
-import csv from 'csvtojson';
-import path from 'path';
-import fs from 'fs'
+import csv from "csvtojson";
+import path from "path";
+import fs from "fs";
 
-const csvFilePath = path.resolve(__dirname, '..', 'data.csv');
-const jsonFilePath = path.resolve(__dirname, '..', 'public', 'data.json');
-
+const csvFilePath = path.resolve(__dirname, "..", "data.csv");
+const jsonFilePath = path.resolve(__dirname, "..", "public", "data.json");
 
 csv()
-.fromFile(csvFilePath)
-.then((rows)=>{
-  rows = rows.map((row) => {
-    row.id = new Number(row.id)
-    row.name = new String(row.name)
-    row.link = new String(row.link)
-    row.isSold = new Boolean(row.isSold)
-    return row
-  })
-  fs.writeFile(jsonFilePath, JSON.stringify(rows, null, 2), (err: any) => {
-    if (err) {
-      throw err;
-    }
-    console.log("JSON generated.");
-  })
-})
+    .fromFile(csvFilePath)
+    .then((rows) => {
+        rows = rows.map((row) => {
+            row.id = new Number(row.id);
+            row.name = new String(row.name);
+            row.link = new String(row.link);
+            row.isSold = new Boolean(row.isSold);
+            return row;
+        });
+        fs.writeFile(
+            jsonFilePath,
+            JSON.stringify(rows, null, 2),
+            (err: any) => {
+                if (err) {
+                    throw err;
+                }
+                console.log("JSON generated.");
+            }
+        );
+    });
--- tools/serve.ts
+++ tools/serve.ts
@@ -1,6 +1,6 @@
-import Koa from 'koa';
-import Router from 'koa-router';
-import fs from 'fs'
+import Koa from "koa";
+import Router from "koa-router";
+import fs from "fs";
 
 let port = Number.parseInt(process.env.PORT ?? "");
 if (Number.isNaN(port)) {
@@ -11,19 +11,17 @@ const host = process.env.HOST ?? "localhost";
 const app = new Koa();
 const router = new Router();
 
-router.get("/data.json", async ctx => {
-  ctx.set("Content-Type", "application/json; charset=utf-8");
-  ctx.body = fs.createReadStream("public/data.json");
+router.get("/data.json", async (ctx) => {
+    ctx.set("Content-Type", "application/json; charset=utf-8");
+    ctx.body = fs.createReadStream("public/data.json");
 });
 
-router.get("/", async ctx => {
-  ctx.set("Content-Type", "text/html; charset=utf-8");
-  ctx.body = fs.createReadStream("public/index.html");
+router.get("/", async (ctx) => {
+    ctx.set("Content-Type", "text/html; charset=utf-8");
+    ctx.body = fs.createReadStream("public/index.html");
 });
 
-app
-    .use(router.routes())
-    .use(router.allowedMethods());
+app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(port, host);
 
